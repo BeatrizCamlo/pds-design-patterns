e914dbf16df2de4bb675d6ec5db00475213dca44
beatriz-camilo <abeatrizcamilo@gmail.com>
2026-09-21 11:37:04 -0300

    testando funcionamento do comportamento speak em cat, dog e horse

--- visitor/Main.java
+++ visitor/Main.java
@@ -5,8 +5,11 @@ public class Main {
 
         AnimalVisitor speak = new Speak();
         Dog casemiro = new Dog("casemiro", true);
-
+        Cat gilson = new Cat("gilson", true);
+        Horse peDePano = new Horse("pe de pano", false);
         casemiro.accept(speak);
+        gilson.accept(speak);
+        peDePano.accept(speak);
 
     }
 }
